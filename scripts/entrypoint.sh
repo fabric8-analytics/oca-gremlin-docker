10e9e6cd165b040f6774e8d3756deb10dc6bd382
Raj Zalavadia <rzalavad@redhat.com>
2020-01-07 12:53:04 +0530

    added advance configuration as per sugession

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,6 +8,13 @@ GREMLIN_CONF=${SERVER_DIR}/conf/gremlin-server/gremlin-server.yaml
 export JAVA_OPTIONS=${JAVA_OPTIONS:- -Xms512m -Xmx1400m}
 echo "Proceeding with JAVA_OPTIONS=$JAVA_OPTIONS"
 
+sed -i.bckp 's#consoleReporter: .*#consoleReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#csvReporter: .*#csvReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#jmxReporter: .*#jmxReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#slf4jReporter: .*#slf4jReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#gangliaReporter: .*#gangliaReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#graphiteReporter: .*#graphiteReporter: {enabled: false}} #' ${GREMLIN_CONF}
+
 if [ -n "$REST" -o "$REST" == "1" ]; then
     echo "replace channelizer as http"
     sed -i.bckp 's#channelizer: .*#channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer#' ${GREMLIN_CONF}
@@ -18,6 +25,22 @@ sed -i.bckp 's#conf/gremlin-server/janusgraph-cql-es-server.properties#conf/grem
 
 if [ -n "$CASSANDRA_SERVICE" ]; then
     sed -i.bckp 's#storage.hostname=.*#storage.hostname='${CASSANDRA_SERVICE}'#' ${PROPS}
+fi
+
+if [ -n "$RESPONSE_TIMEOUT" ]; then
+    sed -i.bckp 's#serializedResponseTimeout: .*#serializedResponseTimeout: '${RESPONSE_TIMEOUT}'#' ${GREMLIN_CONF}
+fi
+
+if [ -n "$SCRIPT_EVALUATION_TIMEOUT" ]; then
+    sed -i.bckp 's#scriptEvaluationTimeout: .*#scriptEvaluationTimeout: '${SCRIPT_EVALUATION_TIMEOUT}'#' ${GREMLIN_CONF}
+fi
+
+if [ -n "$GREMLIN_POOL" ]; then
+    sed -i.bckp 's#gremlinPool: .*#gremlinPool: '${GREMLIN_POOL}'#' ${GREMLIN_CONF}
+fi
+
+if [ -n "$THREAD_POOL" ]; then
+    sed -i.bckp 's#threadPoolWorker: .*#threadPoolWorker: '${THREAD_POOL}'#' ${GREMLIN_CONF}
 fi
 
 cd ${SERVER_DIR}
